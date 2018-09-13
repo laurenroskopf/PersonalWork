f85686c434191bf32647747cc16332645cefe290
Lauren Roskopf <lauren.roskopf@gmail.com>
2018-09-13 13:17:00 -0500

    FirstAndLastNameAndAge

---
+++ src/outputExamples/FirstAndLastNameAndAge.java
@@ -0,0 +1,12 @@
+package outputExamples;
+
+public class FirstAndLastNameAndAge {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Lauren");
+System.out.println("Roskopf");
+System.out.println(16);
+	}
+
+}
