f2afe4e5089327f3678f0b9243352f9e4169a068
Lauren Roskopf <lauren.roskopf@gmail.com>
2018-09-13 14:07:57 -0500

    Likes and Dislikes

---
+++ src/outputExamples/LikesAndDislikes.java
@@ -0,0 +1,16 @@
+package outputExamples;
+
+public class LikesAndDislikes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("Likes"); System.out.print("         "); System.out.println("Dislikes");
+System.out.print("Popcorn");System.out.print("       ");System.out.println("Olives");
+System.out.print("Puppies");System.out.print("       ");System.out.println("Centipedes");
+System.out.print("Art Classes");System.out.print("   "); System.out.println("Spanish");
+System.out.print("Summer");System.out.print("        ");System.out.println("Spring");
+System.out.print("Football");System.out.print("      ");System.out.println("Baseball"); }
+}
+
+
+
