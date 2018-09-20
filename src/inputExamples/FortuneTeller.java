58816c60b727bda38efffa4f84e1f6aaf8fbba91
Lauren Roskopf <lauren.roskopf@gmail.com>
2018-09-20 14:02:30 -0500

    Fortune Teller

---
+++ src/inputExamples/FortuneTeller.java
@@ -0,0 +1,20 @@
+package inputExamples;
+import java.util.Scanner;
+public class FortuneTeller {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int kids= 3;
+int car= 2002;
+int number= 1991;
+Scanner input = new Scanner (System.in);
+System.out.println("Enter the number of kids you have: " + kids);
+System.out.println("Enter the year your current car was made: " + car);
+System.out.println("Enter the year your house was made" + number);
+System.out.println("In 2040 you will have" + kids+"pets");
+System.out.println("You have" + number + "Dollars left because you just spent" + car + " dollars on lottery tickets");
+
+
+	}
+
+}
