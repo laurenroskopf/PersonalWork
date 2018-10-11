42a278cf3378cf88e577bceef7e5acf9d7ec7055
Lauren Roskopf <lauren.roskopf@gmail.com>
2018-10-11 12:55:46 -0500

    Cat

---
+++ src/oop/Cat.java
@@ -0,0 +1,20 @@
+package oop;
+
+public class Cat {
+private int age;
+private double sleep;
+public Cat(){
+	age=6;
+	sleep=12.0;
+}
+public int birthday(){
+	return age+1;}
+public double sleepMore(){
+	return sleep+1;
+}
+public double sleepLess(){
+	return sleep-1;
+}
+	
+}
+
